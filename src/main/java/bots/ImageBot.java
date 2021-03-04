057133f9ffac8358c6e4c4a9b046222848e430fd
User <User@DESKTOP-FOASM6G>
2021-03-04 14:42:43 +0100

    image reaction

--- src/main/java/bots/ImageBot.java
+++ src/main/java/bots/ImageBot.java
@@ -1,24 +1,27 @@
 package bots;
 
+import java.util.Map;
+
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.rest.util.Color;
 
-public class CuteBot {
+public class ImageBot {
 	static GatewayDiscordClient client = DiscordClientBuilder.create("ODE2MjMxNTg2NTY2ODk3Njc0.YD385w.blC0wtWw41lCYqoz7nY-0FuYriE").build()
 			.login().block();
-	
+	static Map<String,String> bildReactions = Map.of(
+			"good bot","https://media.tenor.com/images/6cb817f79a934a3f20d4e1b56d13519c/tenor.gif" ,
+			"$flags", "https://cdn.discordapp.com/attachments/806541558278258748/816011349662367814/unknown.png"
+			);
 	public static void main(String[] args) {
 		client.getEventDispatcher().on(MessageCreateEvent.class).map(MessageCreateEvent::getMessage)
 		.filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
-		.filter(message -> message.getContent().equalsIgnoreCase("good bot")).flatMap(Message::getChannel)
-		.flatMap(channel -> channel.createEmbed(embed -> {
-			embed.setTitle("Thanks bro :wink:");
+		.filter(message -> bildReactions.containsKey(message.getContent()))
+		.flatMap(message -> message.getChannel().block().createEmbed(embed -> {
 			embed.setColor(Color.DISCORD_BLACK);
-			embed.setImage(
-					"https://media.tenor.com/images/6cb817f79a934a3f20d4e1b56d13519c/tenor.gif");
+			embed.setImage(bildReactions.get(message.getContent()));
 		})).subscribe();
 	}
 
